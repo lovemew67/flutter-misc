13d5ea55662106cf7a4c751a29d53d3bca4631a1
Howard Zhou <lovemew67@gmail.com>
2020-02-26 17:40:59 +0800

    task: deploy to iPhone

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
-import './demo.dart';
+import 'app/practice.dart';
+// import 'app/album.dart';
+// import './demo.dart';
 
-void main() => runApp(MyDemoApp());
+// void main() => runApp(MyDemoApp());
+// void main() => runApp(MyAlbumApp());
+void main() => runApp(MyPracticeApp());
