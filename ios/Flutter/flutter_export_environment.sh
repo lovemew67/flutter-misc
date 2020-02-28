294381d65afaf130d716dba9ded1fba028d83f6e
Howard Zhou <lovemew67@gmail.com>
2020-02-29 01:20:35 +0800

    task: update cocoapod

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/zzh/Flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/zzh/Golang/gopath/src/github.com/lovemew67/flutter-misc/mobile"
-export "FLUTTER_TARGET=/Users/zzh/Golang/gopath/src/github.com/lovemew67/flutter-misc/mobile/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/zzh/Golang/gopath/src/github.com/lovemew67/flutter-misc"
+export "FLUTTER_TARGET=/Users/zzh/Golang/gopath/src/github.com/lovemew67/flutter-misc/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/zzh/Flutter/bin/cache/artifacts/engine/ios"
